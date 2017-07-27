ca2a4d46d3eeaa9014fa1bb2f63f02a8b773e777
madandan <madandan@gmail.com>
2017-07-27 18:07:53 +0800

    提交启动页

---
+++ app/src/main/java/com/example/copecloudreader/app/CloudReaderApplication.java
@@ -0,0 +1,17 @@
+package com.example.copecloudreader.app;
+
+import android.content.res.Resources;
+
+/**
+ * Created by madandan on 2017/7/27.
+ */
+
+public class CloudReaderApplication {
+    private static CloudReaderApplication cloudReaderApplication;
+
+    public static CloudReaderApplication getInstance() {
+        return cloudReaderApplication;
+    }
+
+
+}

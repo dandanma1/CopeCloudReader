66c7e126941cbceda263d0675467cad59caf7001
madandan <madandan@gmail.com>
2017-08-01 18:01:03 +0800

    开始编写MainActivity

--- app/src/main/java/com/example/copecloudreader/MainActivity.java
+++ app/src/main/java/com/example/copecloudreader/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.copecloudreader;
 
+import android.databinding.DataBindingUtil;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private FrameLayout llTitleMenu;
+    private Toolbar toolbar;
+    private FloatingActionButton fab;
+    private NavigationView navView;
+    private DrawerLayout drawerLayout;
+    private ViewPager vpContent;
+
+    // 一定需要对应的bean
+    private ActivityMainBinding mBinding;
+    private ImageView llTitleGank;
+    private ImageView llTitleOne;
+    private ImageView llTitleDou;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
